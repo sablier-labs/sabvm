131cbaf0a7646de14447a6cec1341931b593a1e1
Iaroslav Mazur <iaroslav.mazur@proton.me>
2024-05-24 18:06:15 +0300

    feat: burning in SabVM precompile

--- crates/revm/src/inspector/customprinter.rs
+++ crates/revm/src/inspector/customprinter.rs
@@ -373,7 +373,7 @@ mod test {
                 tx.caller = caller_contract;
                 tx.transact_to = TransactTo::Call(SABVM_PRECOMPILE_ADDRESS);
 
-                //Compose the Tx Data, as follows: the BALANCEOF id + address + asset_id
+                //Compose the Tx Data, as follows: the MINT id + recipient + sub_id + amount
                 const MINT_ID: u8 = 0xC0;
                 let recipient = caller_contract;
                 
@@ -382,7 +382,8 @@ mod test {
                 concatenated.append(sub_id.to_be_bytes_vec().as_mut());
                 concatenated.append(amount_to_mint.to_be_bytes_vec().as_mut());
                 tx.data = Bytes::from(concatenated);
-            }).build();
+            })
+            .build();
 
         let tx_result = evm.transact_commit();
         assert!(tx_result.is_ok());
@@ -393,5 +394,56 @@ mod test {
         let minted_asset_id = asset_id_address(caller_contract, sub_id);
         let caller_minted_asset_balance = evm.balance(minted_asset_id, caller_contract).unwrap().0;
         assert_eq!(caller_minted_asset_balance, amount_to_mint);
+    }
+
+    #[test]
+    fn ask_precompile_to_burn() {
+        use crate::sabvm_precompile::ADDRESS as SABVM_PRECOMPILE_ADDRESS;
+        
+        use crate::primitives::{
+            address, asset_id_address, bytes, TransactTo, Bytes, keccak256, AccountInfo, Bytecode, U256,
+        };
+        let caller_contract = address!("5fdcca53617f4d2b9134b29090c87d01058e27e9");
+        let sub_id = U256::from(2);
+        let minted_asset_id = asset_id_address(caller_contract, sub_id);
+        let caller_initial_balance = U256::from(1000);
+        let amount_to_burn = U256::from(500);
+
+        let mut evm = Evm::builder()
+            .with_db(InMemoryDB::default())
+            .modify_db(|db| {
+                db.asset_ids.push(minted_asset_id);
+
+                let code = bytes!("5b597fb075978b6c412c64d169d56d839a8fe01b3f4607ed603b2c78917ce8be1430fe6101e8527ffe64706ecad72a2f5c97a95e006e279dc57081902029ce96af7edae5de116fec610208527f9fc1ef09d4dd80683858ae3ea18869fe789ddc365d8d9d800e26c9872bac5e5b6102285260276102485360d461024953601661024a53600e61024b53607d61024c53600961024d53600b61024e5360b761024f5360596102505360796102515360a061025253607261025353603a6102545360fb61025553601261025653602861025753600761025853606f61025953601761025a53606161025b53606061025c5360a661025d53602b61025e53608961025f53607a61026053606461026153608c6102625360806102635360d56102645360826102655360ae61026653607f6101e8610146610220677a814b184591c555735fdcca53617f4d2b9134b29090c87d01058e27e962047654f259595947443b1b816b65cdb6277f4b59c10a36f4e7b8658f5a5e6f5561");
+                let caller_info = AccountInfo {
+                    balances: HashMap::from([(minted_asset_id, caller_initial_balance)]),
+                    code_hash: keccak256(&code),
+                    code: Some(Bytecode::new_raw(code.clone())),
+                    nonce: 1,
+                };
+                db.insert_account_info(caller_contract, caller_info);
+            })
+            .modify_tx_env(|tx| {
+                tx.caller = caller_contract;
+                tx.transact_to = TransactTo::Call(SABVM_PRECOMPILE_ADDRESS);
+
+                //Compose the Tx Data, as follows: the BURN id + sub_id + amount
+                const BURN_ID: u8 = 0xC1;
+                
+                let mut concatenated = vec![BURN_ID];
+                concatenated.append(sub_id.to_be_bytes_vec().as_mut());
+                concatenated.append(amount_to_burn.to_be_bytes_vec().as_mut());
+                tx.data = Bytes::from(concatenated);
+            })
+            .build();
+
+        let tx_result = evm.transact_commit();
+        assert!(tx_result.is_ok());
+
+        let execution_result = tx_result.unwrap();
+        assert!(execution_result.is_success());
+
+        let caller_minted_asset_balance = evm.balance(minted_asset_id, caller_contract).unwrap().0;
+        assert_eq!(caller_minted_asset_balance, caller_initial_balance - amount_to_burn);
     }
 }
